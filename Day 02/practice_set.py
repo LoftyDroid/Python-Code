a1f883cc4f8c8b013281068832ac0e54e0b87df0
LoftyDroid <shaikhahmed243@gmail.com>
2023-04-27 11:16:25 +0530

    Practice questions

---
+++ Day 02/practice_set.py
@@ -0,0 +1,37 @@
+
+# Write a python program to add two numbers
+a=10
+b=52
+print("The addition of a+b is", a+b)
+
+#Write a python program to find reminder and Quotient when a number is divide by 2
+c=2
+print("The reminder of b%c is",b%c)
+print("The Quotient of b//c is",b//c)
+
+#Check the type of the variable assigned using input function
+a= input("Enter the your name")
+print(a)
+print(type(a))
+
+#Use Comerison operation to find out whether a given
+A = 34
+B = 80
+C = (A>=B)
+print(C)
+
+#A Python program to find average of two numbers enterd by the user
+m = input("Enter first numbers:")
+m = int(m)
+
+n = input("Enter first numbers:")
+n = int(n)
+
+p = (m+n)/2
+print(p)
+
+# Write a python program to calculate square of a number enterd by the uers
+x = input("Enter a number to find it's square:")
+x = int(x)
+y = x*x
+print("The square of the given number is", y)
