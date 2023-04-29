988b1be01deba9df7fbcfc1feda59be5db99e6f6
LoftyDroid <shaikhahmed243@gmail.com>
2023-04-29 22:27:58 +0530

    Day 3 complete

---
+++ Day 03/ps_q3.py
@@ -0,0 +1,5 @@
+'''
+Wriye a program to delet double space in a steing
+'''
+a= "Coding tells a machine which actions to perform and how to  complete tasks.Programming languages provide the rules for building websites, apps, and other computer-based technologies."
+print(a.find("  "))
